1deff2c77a960bea85175739f5a6ede4ee550da4
acampos91c@gmail.com <acampos91c@gmail.com>
2024-08-28 21:08:43 +0000

    int_campaigns_day check

---
+++ models/Intermediate/int_campaigns_day.sql
@@ -0,0 +1,8 @@
+SELECT
+    date_date,
+    ROUND(SUM(ads_cost),2) AS ads_cost,
+    ROUND(SUM(impression),2) as ads_impression,
+    ROUND(SUM(click),2) as ads_clicks
+FROM {{ref('int_campaign')}}
+GROUP BY
+    date_date

498dbf3ed29b5bd2ae04a1f625d5aff9107daff7
acampos91c@gmail.com <acampos91c@gmail.com>
2024-08-28 17:17:38 +0000

    Arreglado hasta finance_days

--- models/Intermediate/int_orders_operational.sql
+++ models/Intermediate/int_orders_operational.sql
@@ -1,34 +1,15 @@
-with margin_ship as(
-
-select
-    orders_id,
-    date_date,
-    margin,
-    shipping_fee,
-    CAST(ship_cost AS float64) AS ship_cost,
-    logcost,
-    quantity,
-    revenue,
-    purchase_cost
-from
-    {{ref("init_sales_margin")}}
-join
-    {{ref("stg_raw__ship")}}
-using(orders_id)
-)
-select
-    orders_id,
-    date_date,
-    margin,
-    shipping_fee,
-    logcost,
-    ship_cost,
-    ROUND(margin+shipping_fee-logcost-ship_cost,2) AS operational_margin,
-    quantity,
-    revenue,
-    purchase_cost
-  
-from
-    margin_ship
-ORDER BY
-    orders_id desc
+SELECT
+  o.orders_id
+  ,o.date_date
+  ,ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin 
+  ,o.quantity
+  ,o.revenue
+  ,o.purchase_cost
+  ,o.margin
+  ,s.shipping_fee
+  ,s.logcost
+  ,CAST(s.ship_cost AS FLOAT64) AS ship_cost
+FROM {{ref("int_orders_margin")}} o
+LEFT JOIN {{ref("stg_raw__ship")}} s 
+  USING(orders_id)
+ORDER BY orders_id desc
